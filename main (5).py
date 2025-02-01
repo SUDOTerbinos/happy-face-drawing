62e49bfdd29f06f9cb86a0956334177100ceb9c4
SUDOTerbinos <terbinosabebebirbirsa@gmail.com>
2025-02-01 16:05:07 +0300

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,14 @@
+def draw_happy_face():
+    face = """
+        *****
+      *       *
+    *   O   O   *
+    *     ^     *
+    *   \___/   *
+      *       *
+        *****
+    """
+    print(face)
+
+if __name__ == "__main__":
+    draw_happy_face()
